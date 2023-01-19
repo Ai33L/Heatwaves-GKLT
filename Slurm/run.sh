8e2bb68adf50f547cff0cf0c35014af720952165
Abel <abels2000@gmail.com>
2023-01-19 17:01:19 +0530

    clean

--- Slurm/run.sh
+++ Slurm/run.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
-# remove old slurm files
+# Function to run an experiment
+# Arguments-
+# 1 - Directory to run experiment in
+# 2 - Value of selection coefficient to be used
+# 3 - Number of trajectories to be run
+# 4 - Directory of initial states
+# 5 - Starting iteration for an experiment (optional - restart from crash) 
+run_exp(){
+
+# check restart argument and set it to 1 if not passed
+iter=${5:-1}
+
+# clear slurm logs
 rm -r *.sh.o*
 rm -r *.sh.e*
 rm GKTL_log.txt
 
-# make a new directory for experiment
+# run GKTL_init for new experiments 
+if [ $iter -eq 1 ]
+then
+echo "starting new experiment"
+# create directory for experiment
 rm -r $1
 mkdir $1
 
-# GKTL run init and check for log
+# run GKTL_init
 pass=0
 while [ $pass -ne 1 ]
 do
@@ -20,27 +36,34 @@ sleep 1
 fi
 done
 
-x=`find $1 -name 'link' |wc -l`
+else
+
+echo "starting experiment from iteration $iter"
+rm -r $1/pass_traj*
+
+fi
+
+# proceed only if pass_init file is found in direcotry
+x=`find $1 -name 'pass_init' |wc -l`
 while [ $x -ne 1 ]
 do
 sleep 60
-x=`find $1 -name 'link' |wc -l`
+x=`find $1 -name 'pass_init' |wc -l`
 done
 
+## end of initialisation - iteration begin
 
-# number of parallel trajectories
-n=4
-
+n=40 # number of parallel trajectories
 
 # loop over algorithm iterations
-for (( k = 1; k <= 16 ; k++ ))
+for (( k = iter; k <= 16 ; k++ ))
 do
 
 echo $k
 
+# submit jobs - n jobs at a time
 c=1
 x_prev=0
-# submit inital set of jobs
 for (( i = 1; i <= $n; i++ )) 
 do 
 
@@ -55,13 +78,11 @@ else
 sleep 1
 fi
 done
-
 done
 
-# submit new jobs as old ones complete
 while [ $c -le $3 ]
 do
-x=`find $1 -name 'traj_new*' |wc -l`
+x=`find $1 -name 'pass_traj*' |wc -l`
 
 for (( i = 0; i < x-x_prev; i++ ))
 do
@@ -87,14 +108,14 @@ sleep 120
 done
 
 # wait till al trajectory runs are completed
-x=`find $1 -name 'traj_new*' |wc -l`
+x=`find $1 -name 'pass_traj*' |wc -l`
 while [ $x -ne $3 ]
 do
 sleep 120
-x=`find $1 -name 'traj_new*' |wc -l`
+x=`find $1 -name 'pass_traj*' |wc -l`
 done
 
-# run resampling for each iteration and check log
+# run resampling and check completion
 pass=0
 while [ $pass -ne 1 ]
 do
@@ -105,11 +126,11 @@ sleep 1
 fi
 done
   
-x=`find $1 -name 'resample_log*' |wc -l`
+x=`find $1 -name 'pass_resample*' |wc -l`
 while [ $x -ne $k ]
 do
 sleep 120
-x=`find $1 -name 'resample_log*' |wc -l`
+x=`find $1 -name 'pass_resample*' |wc -l`
 done 
 
 rm -r *.sh.o*
@@ -127,14 +148,35 @@ sleep 1
 fi
 done
 
-x=`find $1 -name 'prob' |wc -l`
+x=`find $1 -name 'pass_wrap' |wc -l`
 while [ $x -ne 1 ]
 do
 sleep 120
-x=`find $1 -name 'prob' |wc -l`
+x=`find $1 -name 'pass_wrap' |wc -l`
 done
 
 rm -r *.sh.o*
 rm -r *.sh.e*
+rm GKTL_log.txt
 
-echo 'done'
+}
+
+#echo 'K_10_1'
+#echo '---'
+#run_exp 'K_10_1' 10 512 'initial_summer_0.5_6'
+
+#echo 'K_20_1'
+#echo '---'
+#run_exp 'K_20_1' 20 512 'initial_summer_0.5_6'
+
+#echo 'K_40_1'
+#echo '---'
+#run_exp 'K_40_1' 40 512 'initial_summer_0.5_6'
+
+#echo 'K_50_1'
+#echo '---'
+#run_exp 'K_50_1' 50 512 'initial_summer_0.5_6'
+
+echo 'K'
+echo '---'
+run_exp 'K' 0 512 'initial_summer_0.5_6'
